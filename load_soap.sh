8a5e4b4729ad010712423d8e207c011b234b9d92
ivanfoo <sanfru.ivan@gmail.com>
2015-06-03 10:44:11 +0200

    Loading soap.jar for a specific scheme

--- load_soap.sh
+++ load_soap.sh
@@ -1,24 +1,33 @@
 #! /bin/bash
 
-schema=$1
+user=$1
 passw=$2
 
-if [ -z $schema ]; then
-    echo "Usage: load_soap SCHEMA"
+if [ -z $user ]; then
+    echo "Usage: load_soap USER [PASSW]"
     exit 1
 fi
 
 if [ -z $passw ]; then
-    passw=$schema
+    passw=$user
 fi
 
-OC4J_HOME=/tmp/oc4j_extended_101350
+## CONFIG ##
+tmp_dir='/tmp/load_soap'
+file='oc4j_extended_101350.zip'
+remote_file="ftp://ftp.ocu.es/OracleDB/$file"
+## END CONFIG ##
 
-loadjava -f -v -r -genmissing -u ${schema}/${passw} $OC4J_HOME/j2ee/home/lib/ejb.jar
-loadjava -f -v -r -genmissing -u ${schema}/${passw} $OC4J_HOME/j2ee/home/lib/activation.jar
-loadjava -f -v -r -genmissing -u ${schema}/${passw} $OC4J_HOME/j2ee/home/lib/mail.jar
-loadjava -f -v -r -genmissing -u ${schema}/${passw} $OC4J_HOME/j2ee/home/lib/servlet.jar
-loadjava -f -v -r -genmissing -u ${schema}/${passw} $OC4J_HOME/lib/dms.jar
-loadjava -f -v -r -genmissing -u ${schema}/${passw} $OC4J_HOME/webservices/lib/soap.jar
+mkdir $tmp_dir
+curl $remote_file -o $tmp_dir
+unzip $tmp_dir/$file -d $tmp_dir/${file%%.*}
+OC4J_HOME=$tmp_dir/${file%%.*}
+
+loadjava -f -v -r -genmissing -u ${user}/${passw} $OC4J_HOME/j2ee/home/lib/ejb.jar
+loadjava -f -v -r -genmissing -u ${user}/${passw} $OC4J_HOME/j2ee/home/lib/activation.jar
+loadjava -f -v -r -genmissing -u ${user}/${passw} $OC4J_HOME/j2ee/home/lib/mail.jar
+loadjava -f -v -r -genmissing -u ${user}/${passw} $OC4J_HOME/j2ee/home/lib/servlet.jar
+loadjava -f -v -r -genmissing -u ${user}/${passw} $OC4J_HOME/lib/dms.jar
+loadjava -f -v -r -genmissing -u ${user}/${passw} $OC4J_HOME/webservices/lib/soap.jar
 
 exit 0
